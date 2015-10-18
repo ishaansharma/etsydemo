f9b916457a26933939e9a37740456375706cce10
ishaansharma <officialishaansharma@gmail.com>
2015-10-18 11:14:23 +0530

    customize devise forms and alert messages!

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,22 @@
+module DeviseHelper
+	def devise_error_message!
+		return '' if resource.errors.empty?
+
+		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+		sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+		 html = <<-HTML
+    <div class="alert alert-danger alert-dismissable">
+      <h2>#{sentence}</h2>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  
+
+end
